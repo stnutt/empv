a666a5eee38caaa3ed3903d9af30999d6138a27f
Stephen Nutt <stnutt@gmail.com>
2017-12-13 13:00:25 -0600

    Add JSON library

---
+++ empv-json.el
@@ -0,0 +1,44 @@
+;;; empv-json.el --- empv JSON functions             -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017  Stephen Nutt
+
+;; Author: Stephen Nutt <stnutt@gmail.com>
+;; Keywords: multimedia
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Functions for reading and encoding JSON for empv
+
+;;; Code:
+
+(require 'json)
+
+(defun empv-json-read-from-string (string)
+  "Return the object for the JSON-encoded STRING."
+  (let ((json-object-type 'alist)
+        (json-array-type 'list)
+        (json-key-type 'symbol)
+        (json-false nil)
+        (json-null nil))
+    (ignore-errors
+      (json-read-from-string string))))
+
+(defun empv-json-encode (object)
+  "Return the JSON-encoded string for the OBJECT."
+  (json-encode object))
+
+(provide 'empv-json)
+;;; empv-json.el ends here
---
+++ test/empv-json-test.el
@@ -0,0 +1,16 @@
+(require 'empv-json)
+(require 'ert)
+
+(ert-deftest empv-json-test-read ()
+  "Verify an object read from a JSON-encoded string."
+  (should (equal (empv-json-read-from-string "{\"array\":[null,false]}")
+                 '((array nil nil)))))
+
+(ert-deftest empv-json-test-read-invalid ()
+  "Verify nil read from invalid JSON string."
+  (should (null (empv-json-read-from-string "{"))))
+
+(ert-deftest empv-json-test-encode ()
+  "Verify a string JSON-encoded from an object."
+  (should (equal (empv-json-encode '(:array (nil :json-false)))
+                 "{\"array\":[null,false]}")))
